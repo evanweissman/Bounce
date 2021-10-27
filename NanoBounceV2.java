bede098b4d05edd74b62d717a6886000b9e35b8f
evanweissman <evan@weissman.com>
2021-10-26 20:01:16 -0400

    21 lines.

--- NanoBounceV2.java
+++ NanoBounceV2.java
@@ -1,19 +1,8 @@
 public class NanoBounceV2 {
 	public static void main(String[] args) throws InterruptedException {
 		for(int i = 0; i < 31; i++) {
-			if(i%10 == 0) {
-				ball(i, 1);
-			} else if (i%10 == 1 || i%10 == 9) {
-				ball(i, 2);
-			} else if(i%10 == 2 || i%10 == 8) {
-				ball(i, 3);
-			} else if(i%10 == 3 || i%10 == 7) {
-				ball(i, 4);
-			} else if(i%10 == 4 || i%10 == 6) {
-				ball(i, 5);
-			} else {
-				ball(i, 6);
-			} if(i%10 == 0 || i%10  == 5) {
+			ball(i, i%10 == 0 ? 1 : i%10 == 1 || i%10 == 9 ? 2 : i%10 == 2 || i%10 == 8 ? 3 : i%10 == 3 || i%10 == 7 ? 4 : i%10 == 4 | i%10 == 6 ? 5 : 6);
+			if(i%10 == 0 || i%10  == 5) {
 				System.out.print("\u0007");
 			}
 		}
